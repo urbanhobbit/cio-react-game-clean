af901542c5a530f9f27d03f39a6f5a50275d3e3e
urbanhobbit <33397688+urbanhobbit@users.noreply.github.com>
2025-11-16 17:52:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/cio-react/',   // repo adı neyse o
+  base: '/cio-react-game-clean/', // Örn: /cio-react-game-clean/
   plugins: [react()],
 })
